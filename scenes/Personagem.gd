b2f709ca4d7108aa2f3d6f6194d9e3550deb03d7
3R11K <124263623+3R11K@users.noreply.github.com>
2023-02-08 15:24:28 -0300

    pergunta 1

--- scenes/Personagem.gd
+++ scenes/Personagem.gd
@@ -1,10 +1,14 @@
 extends KinematicBody2D
 
 var speed = 200
+#velocidade de movimento do personagem
 var move = Vector2()
+#define o movimento em 2 eixos
 
 func _physics_process(delta):
+#da inicio a fsica do jogo
 	
+	#controles do personagem
 	if Input.is_action_pressed("ui_right"):
 		move.x = speed
 	elif Input.is_action_pressed("ui_left"):
@@ -18,3 +22,11 @@ func _physics_process(delta):
 		move.y = 0
 
 	move_and_slide(move)
+
+
+func _on_Area2D_area_entered(area):
+	get_tree().change_scene("res://scenes/Menu.tscn")
+
+
+func _on_Area2D_body_entered(body):
+	get_tree().change_scene("res://scenes/pergunta2.tscn")
