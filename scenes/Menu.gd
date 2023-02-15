70f8cfdeae7d1fb6471e5dc03c7c4e0d6df102ef
3R11K <124263623+3R11K@users.noreply.github.com>
2023-02-14 21:29:16 -0300

    menu + sprites

--- scenes/Menu.gd
+++ scenes/Menu.gd
@@ -3,9 +3,6 @@ extends Node2D
 func _ready():
 	$AudioStreamPlayer2D.play(true)
 		
-func _on_start_pressed():
+
+func _on_TextureButton_pressed():
 	get_tree().change_scene("res://scenes/conversa 1.tscn")
-
-
-func _on_Button_pressed():
-	get_tree().change_scene("res://scenes/acertou.tscn")
