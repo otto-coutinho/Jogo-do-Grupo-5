08100b3175831802ced1152709d96c39c476ad6b
Rodrigo Sales <freirerodrigosales@gmail.com>
2023-02-15 23:25:44 -0300

    Player perde quando toca o carro no crossyroads

--- scenes/carro esquerda.gd
+++ scenes/carro esquerda.gd
@@ -1,16 +1,15 @@
 extends KinematicBody2D
 
-var speed = 250
+var speed = 100
 var temp = randf()
 
-
-func _process(delta):
-	position.x = position.x - speed  * delta
-	
-	if position.x < -get_viewport().size.x:
-		yield(get_tree().create_timer(temp), "timeout")
-		position.x = 400
+func _ready():
+	set_physics_process(true)
 
 
-func _on_Area2D_body_entered(body):
-	get_tree().reload_current_scene()
+func _physics_process(delta):
+	pass
+
+
+#func _on_Area2D_body_entered(body):
+#	get_tree().reload_current_scene()
---
+++ scenes/crossingroad_scene.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var car_scene = preload("res://scenes/carro.tscn")
+var car
+
+func _ready():
+	car = car_scene.instance()
+	add_child(car)
+	car.set_position(Vector2(205, 180))
+	car.set_z_index(1)
+
+func _process(delta):
+	car.position.x -= 1
